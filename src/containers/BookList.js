ef6449da2a1923398cff1f943e4e51080b896483
ja <ivananl@yahoo.com>
2021-08-20 11:27:14 +0200

    Add style for book component

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -22,20 +22,11 @@ const BookList = () => {
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Book ID</th>
-          <th>Title</th>
-          <th>Category</th>
-        </tr>
-      </thead>
-      <tbody>
-        {list.map((book) => (
-          <Book book={book} key={book.id} handleRemoveBook={handleRemoveBook} />
-        ))}
-      </tbody>
-    </table>
+    <div className="books-div">
+      {list.map((book) => (
+        <Book book={book} key={book.id} handleRemoveBook={handleRemoveBook} />
+      ))}
+    </div>
   );
 };
 
